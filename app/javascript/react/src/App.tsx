517fa239c9499aa8bb50625735c4769461b49fbd
anx450z <anx450z@gmail.com>
2022-09-17 23:06:30 +0530

    add login route

--- app/javascript/react/src/App.tsx
+++ app/javascript/react/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import Welcome from './components/Welcome'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './pages/Navbar'
 import Homepage from './pages/Homepage'
+import LoginPage from './pages/LoginPage'
+import Test from './pages/Test'
 
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
       </BrowserRouter>
     </>
